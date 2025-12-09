d9ce855066d5c0b7aca0268b68e4d46f3bd8df5a
Anniballi-Andrea <andreanniballi@gmail.com>
2025-12-09 15:48:47 +0100

    milestone 3 done

--- controllers/postController.js
+++ controllers/postController.js
@@ -16,7 +16,18 @@ const index = (req, res) => {
 
 const show = (req, res) => {
     const id = Number(req.params.id)
-    res.send(`your trying modify a part of a single post with id: ${req.params.id}`)
+
+    const sql = 'SELECT * FROM posts WHERE id = ?'
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: true, message: 'err.message' })
+        } else if (results.length === 0) {
+            return res.status(404).json({ error: true, message: 'Post Not Found' })
+        }
+        res.json(results)
+    })
+
 
 }
 
